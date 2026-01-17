3ab60908516ca94b6baf272c940bedb7e14be00f
Boris Mita <borisleonel12@gmail.com>
2026-01-17 01:17:45 -0400

    feat: :sparkles: update profile image and enhance personal description

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
   name: "Boris Leonel",
-  profile: "/profile.png",
+  profile: "/profile-2.png",
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Desarrollador Full Stack Junior, con experiencia en frontend utilizando React, Next.js y Material UI, backend con NestJS, y desarrollo móvil con Flutter. Especializado en interfaces intuitivas, componentes reutilizables y APIs estructuradas, con enfoque en código limpio, escalable y eficiente.",
   email: "abusaid7388@gmail.com",
   phone: "+8801608797655",
   address: "Middle Badda, Dhaka, Bangladesh - 1212 ",
